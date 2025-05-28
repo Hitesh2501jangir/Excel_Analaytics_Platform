45259a8806d45c49a231dd2d8c15ae13033a730f
Hitesh Jangir <jangirhitesh2501@gmail.com>
2025-05-28 21:59:52 +0530

    authentication_page_setup

---
+++ Excel_Analaytics_Platform/Backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Routes
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
+
+// DB + Server
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT}`));
+  })
+  .catch(err => console.log(err));
